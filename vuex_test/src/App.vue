1aece004e3bc3e207c970bbf619ed6c54248ccbd
Diego Silva Pires <dpires@spektrumedia.com>
2017-10-25 18:05:15 -0400

    vuex

---
+++ vuex_test/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="hero is-info is-fullheight">
+    <div class="hero-head">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item">
+              <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenuHeroB">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id="navbarMenuHeroB" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item is-active">
+                Home
+              </a>
+              <a class="navbar-item">
+                Examples
+              </a>
+              <a class="navbar-item">
+                Documentation
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <div class="hero-body">
+      <div class="container has-text-centered">
+       
+       <router-view  />
+       
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  @import "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.css";
+
+</style>
---
+++ vuex_test/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <a @click="increment">asd</a>
+  </div>
+</template>
+
+<script>
+import store from '@/store'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods: {
+    increment: function () {
+      store.commit('increment')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
